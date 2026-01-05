c0b09b9ee793aebef7fca1beea72f38e14668be6
CyberPanther232 <kinneyh27@gmail.com>
2026-01-04 20:01:39 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	deleted:    Scanning/portscan/target/.rustc_info.json
    	deleted:    Scanning/portscan/target/CACHEDIR.TAG
    	deleted:    Scanning/portscan/target/debug/.cargo-lock
    	deleted:    Scanning/portscan/target/debug/.fingerprint/portscan-098b0a271d3e8775/invoked.timestamp
    	deleted:    Scanning/portscan/target/debug/.fingerprint/portscan-098b0a271d3e8775/output-test-bin-portscan
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.1zswu8sgxdfwzv83qhco3oma3.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.2o6274how7rfh1e84kcgztbr0.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.5qrbitomb15tpbz9rq0jwmus6.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.68blsou0lfu4uo5ayn6mqnsqe.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.7fysh6mva5w7ju0qc14hu2lir.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.btkoq6x4xsp90vhqghdy4vmf5.12lzrcc.rcgu.o
    	deleted:    Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.d
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/1zswu8sgxdfwzv83qhco3oma3.o
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/2o6274how7rfh1e84kcgztbr0.o
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/5qrbitomb15tpbz9rq0jwmus6.o
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/68blsou0lfu4uo5ayn6mqnsqe.o
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/btkoq6x4xsp90vhqghdy4vmf5.o
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/dep-graph.bin
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/query-cache.bin
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q-6vcj1avjk0skc5ykhvqbmmmh0/work-products.bin
    	deleted:    Scanning/portscan/target/debug/incremental/portscan-1aje83msm1qnk/s-hejwh5otk3-0wgl34q.lock
    
    Untracked files:
    	IoCMCP/.venv/
    	Scanning/portscan/target/

--- Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.d
+++
@@ -1,5 +0,0 @@
-C:\Users\kinne\Desktop\Development\Blue-team-tools\Scanning\portscan\target\debug\deps\portscan-098b0a271d3e8775.d: src\main.rs
-
-C:\Users\kinne\Desktop\Development\Blue-team-tools\Scanning\portscan\target\debug\deps\portscan-098b0a271d3e8775.exe: src\main.rs
-
-src\main.rs:
