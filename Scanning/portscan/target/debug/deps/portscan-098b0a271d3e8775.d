5b8979f68b9e3edc6902e9647530ded7017f2fd9
CyberPanther232 <kinneyh27@gmail.com>
2026-01-04 19:57:43 -0500

    FIX: Added git ignore to remove unnecessary files

---
+++ Scanning/portscan/target/debug/deps/portscan-098b0a271d3e8775.d
@@ -0,0 +1,5 @@
+C:\Users\kinne\Desktop\Development\Blue-team-tools\Scanning\portscan\target\debug\deps\portscan-098b0a271d3e8775.d: src\main.rs
+
+C:\Users\kinne\Desktop\Development\Blue-team-tools\Scanning\portscan\target\debug\deps\portscan-098b0a271d3e8775.exe: src\main.rs
+
+src\main.rs:
